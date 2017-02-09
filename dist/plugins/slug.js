7c1403ddfe9dc6eba1ee3ef8cdc14ab9884b2766
Andrew Bell <andy.bell@informationage.co.nz>
2017-02-09 14:20:05 +1300

    Version Bump from 0.2.2 to 0.2.3

--- dist/plugins/slug.js
+++ dist/plugins/slug.js
@@ -6,18 +6,11 @@ var slugPlugin = function slugPlugin(fn) {
   var slug = function slug(fn, root, node) {
     if (root === node) return '';
 
-    var parent = fn.getParent(fn, root, node);
-    var nodeType = fn.nodeType(fn, parent);
-
-    if (nodeType === 'object') {
-      var value = fn.value(node);
-      var propertyName = value.propertyName;
+    var value = fn.value(node);
+    var propertyName = value.propertyName;
 
 
-      return propertyName;
-    }
-
-    return originalSlug(fn, root, node);
+    return propertyName || originalSlug(fn, root, node);
   };
 
   slug.def = originalSlug.def;
