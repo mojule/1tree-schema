531f24cf2787eff9636043b16cc21b5a0102f4c3
Nik Coughlin <nrkn.com@gmail.com>
2017-05-02 14:28:03 +1200

    Fixes #7

--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@
 
 const is = require( '@mojule/is' )
 const TreeFactory = require( '@mojule/tree' ).Factory
+const FactoryFactory = require( '@mojule/tree' ).FactoryFactory
 const defaultPlugins = require( './plugins' )
-
-const Factory = ( ...plugins ) => {
-  let options = {}
-
-  if( plugins.length > 0 && is.object( plugins[ plugins.length - 1 ] ) )
-    options = plugins.pop()
-
-  if( plugins.length === 1 && is.array( plugins[ 0 ] ) )
-    plugins = plugins[ 0 ]
-
-  plugins = defaultPlugins.concat( plugins )
-
-  return TreeFactory( plugins, options )
-}
-
+const Factory = FactoryFactory( TreeFactory, defaultPlugins )
 const SchemaTree = Factory()
 
 Object.assign( SchemaTree, { Factory } )
